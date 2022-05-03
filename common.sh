3906a5a0e79a68252a68e18ed9f7c4ea51b12857
dileepchandrasony <dileepchandrasony191092@gmail.com>
2022-05-03 18:57:18 +0530

    v1

--- common.sh
+++ common.sh
@@ -3,7 +3,7 @@
 CheckRootUser() {
   USER_ID=$(id -u)
 
-  if[ "$USER_ID" -ne "0"]
+  if [ "$USER_ID" -ne "0" ]
   then
     echo -e "\e[31mYou should run this script as root user only\e[0m"
     exit 1
@@ -14,7 +14,7 @@ LOG_FILE=/tmp/roboshop.log
 rm -f $LOG_FILE
 
 StatusCheck() {
-  if [ $1 eq 0]
+  if [ $1 eq 0 ]
   then
     echo -e "\e[32mSUCCESSFUL\e[0m"
   else
