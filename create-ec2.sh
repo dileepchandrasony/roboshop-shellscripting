1f6f820be69b39de73664f124c0aa306c6b17f13
dileepchandrasony <dileepchandrasony191092@gmail.com>
2022-05-05 17:41:58 +0530

    Updating components

--- create-ec2.sh
+++ create-ec2.sh
@@ -6,6 +6,12 @@ then
   exit 1
 fi
 
+NAME=$1
+
 if [ "$1" == "list" ]
 then
-  
+
+
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --output table | grep ImageId | awk '{print $4}')
+
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$NAME}]"
