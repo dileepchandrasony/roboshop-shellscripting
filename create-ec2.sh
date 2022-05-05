fee5d9eb87a593f6aab3407998551f9ac559cbae
dileepchandrasony <dileepchandrasony191092@gmail.com>
2022-05-05 22:42:45 +0530

    Updating components

--- create-ec2.sh
+++ create-ec2.sh
@@ -18,7 +18,7 @@ fi
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" --output table | grep ImageId | awk '{print $4}')
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$NAME}]" &> /dev/null
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$NAME}]" "ResourceType=instance,Tags=[{Key=Name,Value=$NAME}]" --security-group-ids sg-0d36d07bee578d702 &> /dev/null
 
 echo "Instance $NAME created"
 
