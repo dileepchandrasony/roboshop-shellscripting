237788b31d8469052f0de431b6bb4d97201d8900
dileepchandrasony <dileepchandrasony191092@gmail.com>
2022-05-06 23:46:13 +0530

    Updating components

--- components/frontend.sh
+++ components/frontend.sh
@@ -9,7 +9,7 @@ yum install nginx -y &>> ${LOG_FILE}
 StatusCheck $?
 
 #systemctl start nginx
-echo -e "\n\n"
+echo -e "\n"
 
 ECHO "Downloading frontend code"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> ${LOG_FILE}
@@ -17,32 +17,37 @@ StatusCheck $?
 
 cd /usr/share/nginx/html
 
-echo -e "\n\n"
+echo -e "\n"
 
 ECHO "Removing Old Files"
 rm -rf * &>> ${LOG_FILE}
 StatusCheck $?
 
-echo -e "\n\n"
+echo -e "\n"
 
 ECHO "Extracting Zip Content"
 unzip /tmp/frontend.zip &>> ${LOG_FILE}
 StatusCheck $?
 
-echo -e "\n\n"
+echo -e "\n"
 
 ECHO "Copying Extracted Content"
 mv frontend-main/* . &>> ${LOG_FILE} && mv static/* . &>> ${LOG_FILE} && rm -rf frontend-main README.md &>> ${LOG_FILE}
 StatusCheck $?
 
-echo -e "\n\n"
+echo -e "\n"
 
 ECHO "Copy Roboshop Nginx Config"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> ${LOG_FILE}
 StatusCheck $?
 
-echo -e "\n\n"
+echo -e "\n"
 
+ECHO "Updating Ngnix configuration"
+sed -i -e '/catalogue/ s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+StatusCheck $?
+
+echo -e "\n"
 ECHO "Start Nginx Service"
 systemctl enable nginx &>> ${LOG_FILE} && systemctl restart nginx &>> ${LOG_FILE}
 StatusCheck $?
