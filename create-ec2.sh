ebf3a357b7851144a5e8044afcc3d472c409ead0
dileepchandrasony <dileepchandrasony191092@gmail.com>
2022-05-08 01:09:37 +0530

    Updating components

--- create-ec2.sh
+++ create-ec2.sh
@@ -4,11 +4,11 @@
 for Arg in "$@"
 do
 
-#if [ -z "${Arg}" ]
-#then
-#  echo "Instance name is not passed as an argument"
-#  exit 1
-#fi
+if [ -z "${Arg}" ]
+then
+  echo "Instance name is not passed as an argument"
+  exit 1
+fi
 
 NAME=${Arg}
 
