4f7b351e0205cd94ed0af3f29aecc9c7c8da62ac
dileepchandrasony <dileepchandrasony191092@gmail.com>
2022-05-08 01:32:21 +0530

    Updating components

--- create-ec2.sh
+++ create-ec2.sh
@@ -9,7 +9,7 @@ then
   exit 1
 fi
 
-if [ "${Arg}" == "list" ]
+if [ "$1" == "list" ]
 then
   aws ec2 describe-instances  --query "Reservations[*].Instances[*].{PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress,Name:Tags[?Key=='Name']|[0].Value,Status:State.Name}"  --output table
   exit 0
