1c1a5a27c032f5b70b95e95b031f612925a64588
dileepchandrasony <dileepchandrasony191092@gmail.com>
2022-05-07 22:56:32 +0530

    Updating components

---
+++ components/rabbitmq.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+source common.sh
+
+CheckRootUser
+
+
+ECHO "Setting up rabbitmq Repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>> ${LOG_FILE}
+StatusCheck $?
+
+ECHO "Installing Rabbitmq & Erlang"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>> ${LOG_FILE}
+StatusCheck $?
+
+ECHO "Starting Rabbitmq service"
+systemctl enable rabbitmq-server && systemctl start rabbitmq-server &>> ${LOG_FILE}
+StatusCheck $?
+
+rabbitmqctl list_users | grep roboshop &>> ${LOG_FILE}
+
+if [ $? -ne 0 ]
+then
+ECHO "Creating Application user"
+rabbitmqctl add_user roboshop roboshop123 &>> ${LOG_FILE}
+StatusCheck $?
+fi
+
+ECHO "Setting up Application user permissions"
+rabbitmqctl set_user_tags roboshop administrator &>> ${LOG_FILE} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> ${LOG_FILE}
+StatusCheck $?
+
+
+
+
+
