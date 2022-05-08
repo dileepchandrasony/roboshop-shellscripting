c19a540ecdbed3be29ddcba147cc90ac938c2cc9
dileepchandrasony <dileepchandrasony191092@gmail.com>
2022-05-08 18:27:59 +0530

    Updating components

--- install-component.sh
+++ install-component.sh
@@ -12,22 +12,23 @@ sshpass -f rootpwd.sh ssh -o StrictHostKeyChecking=no root@18.234.82.125
 
 #ECHO "Setting up hostname"
 
-sshpass -f rootpwd.sh ssh -t root@18.234.82.125 'set-hostname frontend;git clone https://github.com/dileepchandrasony/roboshop-shellscripting.git;git pull;cd roboshop-shellscripting;make frontend;exit'
+sshpass -f rootpwd.sh ssh -t root@18.234.82.125 'set-hostname frontend'
+#git clone https://github.com/dileepchandrasony/roboshop-shellscripting.git;git pull;cd roboshop-shellscripting;make frontend;exit'
 #&>> ${LOG_FILE}
 #StatusCheck $?
 
 
 #ECHO "Cloning git repo"
-#sshpass -f rootpwd.sh ssh -t root@18.234.82.125 'git clone https://github.com/dileepchandrasony/roboshop-shellscripting.git;
+sshpass -f rootpwd.sh ssh -t root@18.234.82.125 'git clone https://github.com/dileepchandrasony/roboshop-shellscripting.git'
 
-#sshpass -f rootpwd.sh ssh -t root@18.234.82.125 'git pull'
+sshpass -f rootpwd.sh ssh -t root@18.234.82.125 'git pull'
 ##&>> ${LOG_FILE}
 ##StatusCheck $?
 #
-#sshpass -f rootpwd.sh ssh root@18.234.82.125 'cd roboshop-shellscripting'
+sshpass -f rootpwd.sh ssh -t root@18.234.82.125 'cd roboshop-shellscripting'
 #
-#sshpass -f rootpwd.sh ssh root@18.234.82.125 'make frontend'
+sshpass -f rootpwd.sh ssh -t root@18.234.82.125 'make frontend'
 #
 ##ECHO "logging out"
-#sshpass -f rootpwd.sh ssh root@18.234.82.125 'exit'
+sshpass -f rootpwd.sh ssh -t root@18.234.82.125 'exit'
 ##StatusCheck $?
