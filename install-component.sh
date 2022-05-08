9eff2777389a548967c76fb0ae2252bc69b251de
dileepchandrasony <dileepchandrasony191092@gmail.com>
2022-05-08 13:34:40 +0530

    Updating components

--- install-component.sh
+++ install-component.sh
@@ -4,6 +4,8 @@ component=$1
 
 sshpass -f rootpwd.sh ssh -o StrictHostKeyChecking=no root@54.83.109.215
 
+echo "Instance connected"
+
 set-hostanme ${component}
 
 git clone https://github.com/dileepchandrasony/roboshop-shellscripting.git
