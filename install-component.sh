14391198a07761dd4f632fe2c5519e9b11873d2f
dileepchandrasony <dileepchandrasony191092@gmail.com>
2022-05-08 17:45:38 +0530

    Updating components

--- install-component.sh
+++ install-component.sh
@@ -4,25 +4,27 @@ source common.sh
 
 component=$1
 
-ECHO "Connecting to Instance"
+#ECHO "Connecting to Instance"
 
 sshpass -f rootpwd.sh ssh -o StrictHostKeyChecking=no root@18.234.82.125
 
-StatusCheck $?
+#StatusCheck $?
 
-ECHO "Setting up hostname"
-set-hostname ${component} &>> ${LOG_FILE}
-StatusCheck $?
+#ECHO "Setting up hostname"
+set-hostname ${component}
+#&>> ${LOG_FILE}
+#StatusCheck $?
 
 
-ECHO "Cloning git repo"
-git clone https://github.com/dileepchandrasony/roboshop-shellscripting.git &>> ${LOG_FILE}
-StatusCheck $?
+#ECHO "Cloning git repo"
+git clone https://github.com/dileepchandrasony/roboshop-shellscripting.git
+#&>> ${LOG_FILE}
+#StatusCheck $?
 
 cd roboshop-shellscripting
 
 make ${component}
 
-ECHO "logging out"
+#ECHO "logging out"
 logout
-StatusCheck $?
+#StatusCheck $?
